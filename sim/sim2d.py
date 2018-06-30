b0b6a05223fbbafbe2115f861fdf973d22b3fad3
Daniel Stang <dstang14@gmail.com>
2018-06-30 18:34:42 -0400

    Add zoomed axis and drive in circle option for bonus material.

--- sim/sim2d.py
+++ sim/sim2d.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 import time
 
 
@@ -10,7 +11,6 @@ def sim_run(options, KalmanFilter):
     # Simulator Options
     FIG_SIZE = options['FIG_SIZE'] # [Width, Height]
     DRIVE_IN_CIRCLE = options['DRIVE_IN_CIRCLE']
-
 
     kalman_filter = KalmanFilter()
 
@@ -50,36 +50,45 @@ def sim_run(options, KalmanFilter):
             u_pedal = 0
             u_steer = 0
 
+        if DRIVE_IN_CIRCLE:
+            if t0 < 10:
+                u_pedal = 5
+                u_steer = 0
+            else:
+                u_pedal = 5
+                u_steer = 0.45
+
         x1 = v0*np.cos(theta0)*dt + x0
         y1 = v0*np.sin(theta0)*dt + y0
         v1 =(-v0 + 1.0*u_pedal)/2.0*dt + v0
         theta1 = theta_dot0*dt + theta0
         theta_dot1 = u_steer
 
-
         return [x1, y1, v1, theta1, theta_dot1]
 
     state = []
     est_data_t = []
-    v_est_data = []
     x_est_data = []
+    noise_data = []
     t = np.linspace(0.0,100,1001)
     dt = 0.1
     for t0 in t:
         state += [physics(t0,dt,state)]
-        if t0%1.0 == 0.0:
+        if True:#t0%1.0 == 0.0:
             est_data_t += [t0]
             # Measure car location.
-            state_with_noise = state[-1]+(np.random.rand(1)[0]-0.5)*0.3
+            state_with_noise = []
+            state_with_noise += [state[-1][0]+(np.random.rand(1)[0]-0.5)*0.5]
+            state_with_noise += [state[-1][1]+(np.random.rand(1)[0]-0.5)*0.5]
+            if DRIVE_IN_CIRCLE:
+                state_with_noise += [state[-1][3]+(np.random.rand(1)[0]-0.5)*0.0]
+            noise_data += [state_with_noise]
+
             if t0 == 0.0:
-                x_est_data += [0]
-                v_est_data += [0]
+                x_est_data += [[0,0]]
                 continue
-            x_est_data += [0]
-            v_est_data += [0]
-            #x_est_data += [kalman_filter.predict(t0) - state[-1]]
-            #kalman_filter.measure_and_update(state_with_noise,t0)
-            #v_est_data += [kalman_filter.v]
+            kalman_filter.predict(dt)
+            x_est_data += [kalman_filter.measure_and_update(state_with_noise,dt)]
 
 
     ###################
@@ -96,13 +105,14 @@ def sim_run(options, KalmanFilter):
     ax.set_ylim([0, 100])
     plt.xticks([])
     plt.yticks([])
-    #plt.yticks(np.arange(0,31,3))
     plt.title('Kalman 2D')
 
 
     # Main plot info.
     car, = ax.plot([], [], 'r-', linewidth = 2)
     light, = ax.plot([94,94], [4,2] , 'r-', linewidth = 3)
+    est, = ax.plot([], [], 'bs', markersize=20, fillstyle='none', linewidth=4)
+    meas, = ax.plot([], [], 'gs', markersize=30, fillstyle='none', linewidth=4)
 
     # First section.
     ax.plot([1,1], [9,1], 'k-')
@@ -140,6 +150,53 @@ def sim_run(options, KalmanFilter):
     ax.plot([87,2], [95,95], 'k-')
     ax.plot([2,2], [95,87], 'k-')
 
+    # Zoom.
+    zoom_loc = 6
+    if DRIVE_IN_CIRCLE:
+        zoom_loc = 1
+    axins = zoomed_inset_axes(ax, 6, loc=zoom_loc)
+    car_zoom, = axins.plot([], [], 'r-', linewidth = 3)
+    est_zoom, = axins.plot([], [], 'bs', markersize=5, fillstyle='full', linewidth=4)
+    meas_zoom, = axins.plot([], [], 'gs', markersize=20, fillstyle='none', linewidth=10)
+
+    plt.yticks([])
+    plt.xticks([])
+    # First section.
+    axins.plot([1,1], [9,1], 'k-')
+    axins.plot([1,87], [9, 9], 'k-')
+    axins.plot([1,85], [5,5], 'k--')
+    axins.plot([1,95], [1,1], 'k-')
+    axins.plot([87,87], [9,1], 'k--')
+
+    # First intersection.
+    axins.plot([95,105], [9, 9], 'k-')
+    axins.plot([97,105], [5,5], 'k--')
+    axins.plot([95,105], [1,1], 'k-')
+    axins.plot([95,95], [9,1], 'k--')
+
+    # Second section.
+    axins.plot([87,87], [9, 87], 'k-')
+    axins.plot([91,91], [11, 85], 'k--')
+    axins.plot([95,95], [9, 87], 'k-')
+    axins.plot([87,95], [9,9], 'k--')
+
+    #second intersection.
+    axins.plot([87,95], [87,87], 'k--')
+    axins.plot([87,87], [87,95], 'k--')
+    axins.plot([87,95], [95,95], 'k--')
+
+    axins.plot([87,87], [95, 105], 'k-')
+    axins.plot([91,91], [97, 105], 'k--')
+    axins.plot([95,95], [87, 105], 'k-')
+    axins.plot([92,94], [94,94] , 'g-', linewidth = 3)
+
+
+    # Final Section.
+    axins.plot([87,2], [87,87], 'k-')
+    axins.plot([87,2], [91,91], 'k--')
+    axins.plot([87,2], [95,95], 'k-')
+    axins.plot([2,2], [95,87], 'k-')
+
     def update_plot(num):
         t_loc = int(t[num])
 
@@ -150,7 +207,15 @@ def sim_run(options, KalmanFilter):
         car_sin = np.sin(car_ang)
         car.set_data([car_loc[0], car_loc[0]+2*car_cos],
                         [car_loc[1], car_loc[1]+2*car_sin])
+        car_zoom.set_data([car_loc[0], car_loc[0]+2*car_cos],
+                        [car_loc[1], car_loc[1]+2*car_sin])
+        axins.set_xlim(car_loc[0]-5, car_loc[0]+5)
+        axins.set_ylim(car_loc[1]-5, car_loc[1]+5)
 
+        est.set_data([x_est_data[num][0]],[x_est_data[num][1]])
+        meas.set_data([noise_data[num][0]],[noise_data[num][1]])
+        est_zoom.set_data([x_est_data[num][0]],[x_est_data[num][1]])
+        meas_zoom.set_data([noise_data[num][0]],[noise_data[num][1]])
         if t_loc >= 29:
             light.set_color('green')
 
